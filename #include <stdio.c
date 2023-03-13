603f3379f1c214648190d2387b164d285c97601b
Yvan <Yvan-W@hotmail.com>
2023-03-13 10:11:08 +0800

    1

--- #include <stdio.c
+++ #include <stdio.c
@@ -3,7 +3,12 @@ int main
 {
     int a,b,c,i;
     a=0,b=1;
-    printf ("%d",a);
-    printf ("%d",b);
+    printf ("%d",a);//输出0
+    printf ("%d",b);//输出1
     for (i=1,i<=100,i++)
+    {
+        c=a+b;
+        printf( "%d",c);
+        a=b,b=c;
+    }
 }
